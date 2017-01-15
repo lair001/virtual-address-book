6c29bb3a5dd60aa96df0be4ad3385e4d14dc97e8
Samuel Lair <lair001@gmail.com>
2017-01-15 02:30:29 -0500

    implemented a rescue from RecordNotFound exception in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
 
 	include Pundit
 
+	rescue_from(ActiveRecord::RecordNotFound) { render json: {}, status: 404, serializer: RecordNotFoundSerializer }
 	protect_from_forgery with: :exception
 
 end
