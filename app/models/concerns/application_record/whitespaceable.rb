49179d96f8fe1aabefd8bd16dafc54ea8943b1d5
Samuel Lair <lair001@gmail.com>
2017-01-14 22:43:44 -0500

    imported Forbiddable and Whitespaceable modules from Poetic Rivals

---
+++ app/models/concerns/application_record/whitespaceable.rb
@@ -0,0 +1,16 @@
+class ApplicationRecord
+	module Whitespaceable
+		module ClassMethods
+
+			def trim_whitespace_in(string)
+				string.strip.gsub(/(?<foo>(\s|\u00A0|\u2003))( |\u00A0)/, '\k<foo>'  ).gsub(/( |\u00A0)(?<foo>(\s|\u00A0|\u2003))/, '\k<foo>')
+			end
+
+			def convert_whitespace_that_is_not_spaces_to_spaces_in(string)
+				string.gsub(/[\t\f\v\n\r\u00A0…\u2003]/, " ")
+			end
+
+		end
+
+	end
+end
