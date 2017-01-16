148c5a686db4fe2d13119d59e18c0b13b5af6fd8
Samuel Lair <lair001@gmail.com>
2017-01-16 02:02:45 -0500

    implemented contact serializer and changed user serializer to render a user's contacts

---
+++ app/serializers/contact_serializer.rb
@@ -0,0 +1,3 @@
+class ContactSerializer < ActiveModel::Serializer
+	attributes :id, :user_id, :last_name, :first_name, :email, :phone, :address_line_1, :address_line_2, :city, :state, :zip_code
+end
