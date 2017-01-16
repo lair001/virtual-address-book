111752f405d84eca7e370ab06a12d154889e4a92
Samuel Lair <lair001@gmail.com>
2017-01-16 02:30:05 -0500

    implemented contacts controller

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,36 @@
+class ContactsController < JsonController
+
+	def create
+		@contact = Contact.new(contact_params)
+		@contact.user = current_user
+		if @contact.save
+			render json: @contact
+		else
+			render json: @contact, status: 422, serializer: ModelErrorsSerializer
+		end
+	end
+
+	def update
+		@contact = Contact.find(params[:id])
+		authorize(@contact)
+		if @contact.update(contact_params)
+			render json: @contact
+		else
+			render json: @contact, status: 422, serializer: ModelErrorsSerializer
+		end
+	end
+
+	def destroy
+		@contact = Contact.find(params[:id])
+		authorize(@contact)
+		@contact.destroy
+		render json: {}, serializer: ResourceDestroyedSerializer
+	end
+
+private
+
+	def contact_params
+		params.require(:contact).permit(:last_name, :first_name, :email, :phone, :address_line_1, :address_line_2, :city, :state, :zip_code)
+	end
+
+end
