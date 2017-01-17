5521d4edcb0adc6ef522b213f886f9a4b00ed73e
Samuel Lair <lair001@gmail.com>
2017-01-16 22:11:57 -0500

    angularApp: added sign_in and sign_up levels

--- app/assets/javascripts/angularApp/app.js
+++ app/assets/javascripts/angularApp/app.js
@@ -1,32 +1,46 @@
-angular
-	.module('app', ['ui.router', 'templates'])
-	.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-		$stateProvider
-			.state('index', {
-				url: '/',
-				templateUrl: 'index.html'
-			})
-			.state('index.visitor', {
-				url: 'visitor',
-				templateUrl: 'index/visitor.html',
-			})
-			.state('index.visitor.welcome', {
-				url: '/welcome',
-				templateUrl: 'index/visitor/welcome.html'
-			})
-			.state('index.signed_in', {
-				url: 'signed_in',
-				templateUrl: 'index/signed_in.html'
-			})
-			.state('index.signed_in.contacts', {
-				url: '/contacts',
-				templateUrl: 'index/signed_in/contacts.html'
-			});
+(function() {
 
-			$urlRouterProvider
-				.when("/", "/visitor/welcome")
-				.when("/visitor", "/visitor/welcome")
-				.when("/signed_in", "/visitor/welcome")
-				.otherwise("/visitor/welcome");
+	"use strict";
 
-	}]);
+	angular
+		.module('app', ['ui.router', 'templates'])
+		.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+			$stateProvider
+				.state('index', {
+					url: '/',
+					templateUrl: 'index.html'
+				})
+				.state('index.visitor', {
+					url: 'visitor',
+					templateUrl: 'index/visitor.html',
+				})
+				.state('index.visitor.sign_in', {
+					url: '/sign_in',
+					templateUrl: 'index/visitor/sign_in.html'
+				})
+				.state('index.visitor.sign_up', {
+					url: '/sign_up',
+					templateUrl: 'index/visitor/sign_up.html'
+				})
+				.state('index.visitor.welcome', {
+					url: '/welcome',
+					templateUrl: 'index/visitor/welcome.html'
+				})
+				.state('index.signed_in', {
+					url: 'signed_in',
+					templateUrl: 'index/signed_in.html'
+				})
+				.state('index.signed_in.contacts', {
+					url: '/contacts',
+					templateUrl: 'index/signed_in/contacts.html'
+				});
+
+				$urlRouterProvider
+					.when("/", "/visitor/welcome")
+					.when("/visitor", "/visitor/welcome")
+					.when("/signed_in", "/visitor/welcome")
+					.otherwise("/visitor/welcome");
+
+		}]);
+
+})();
