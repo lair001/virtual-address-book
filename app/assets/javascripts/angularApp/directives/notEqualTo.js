adb27aa458cc706f03eb95a7dc6e725ac4984865
Samuel Lair <lair001@gmail.com>
2017-01-19 04:34:07 -0500

    implemented not equal to directive

---
+++ app/assets/javascripts/angularApp/directives/notEqualTo.js
@@ -0,0 +1,28 @@
+(function() {
+
+    function notEqualTo() {
+        return {
+
+            restrict: 'A',
+            require: "ngModel",
+            scope: {
+                otherModelValueObject: "=notEqualTo"
+            },
+            link: function(scope, element, attributes, ngModel) {
+
+                ngModel.$validators.notEqualTo = function(modelValue) {
+                    return modelValue !== scope.otherModelValueObject.$modelValue;
+                };
+
+                scope.$watch("otherModelValue", function() {
+                    ngModel.$validate();
+                });
+            }
+        };
+    };
+
+    angular
+        .module('app')
+        .directive("notEqualTo", notEqualTo);
+
+})();
