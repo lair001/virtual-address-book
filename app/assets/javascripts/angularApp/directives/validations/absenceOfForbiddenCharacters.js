efeddc14cbaadacacf30140e7828b84f428e6355
Samuel Lair <lair001@gmail.com>
2017-01-19 23:16:21 -0500

    fixed absenceOfForbiddenCharacters directive

--- app/assets/javascripts/angularApp/directives/validations/absenceOfForbiddenCharacters.js
+++ app/assets/javascripts/angularApp/directives/validations/absenceOfForbiddenCharacters.js
@@ -9,7 +9,7 @@
 			link: function (scope, element, attrs, ngModel) {
 
 				ngModel.$validators.absenceOfForbiddenCharacters = function(value) {
-					return /[\s!-ϿԱ-֏ἀ-῾\u2003₠-₾]+$/i.test(value);
+					return /^[\s!-ϿԱ-֏ἀ-῾\u2003₠-₾]+$/i.test(value);
 				};
 
 			}
