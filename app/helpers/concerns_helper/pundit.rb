763b397303f6b1624f0a0cea783e0f194308da4a
Samuel Lair <lair001@gmail.com>
2017-01-19 23:24:45 -0500

    whitelisted users#create for access by visitors

--- app/helpers/concerns_helper/pundit.rb
+++ app/helpers/concerns_helper/pundit.rb
@@ -2,7 +2,7 @@ module ConcernsHelper
 	module Pundit
 
 		def authorize_access_to_app
-			if !['visitor', 'sessions'].include?(controller_name)
+			if !['visitor', 'sessions'].include?(controller_name) && !(controller_name == "users" && action_name == "create")
 				authorize :access, :app?
 			end
 		end
