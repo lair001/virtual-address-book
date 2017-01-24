1d7f2a86c78b6d5b6d3950f1a3b7b1f270fbac73
Samuel Lair <lair001@gmail.com>
2017-01-23 23:35:17 -0500

    secrets.yml removed from git ignore - production base secret key will be stored in a .env file

--- app/assets/javascripts/angularApp/directives/validations/absenceOfForbiddenCharacters.js
+++ app/assets/javascripts/angularApp/directives/validations/absenceOfForbiddenCharacters.js
@@ -8,7 +8,7 @@
 
 				restrict: 'A',
 				require: 'ngModel',
-				link: function (scope, element, attrs, ngModel) {
+				link: function(scope, element, attrs, ngModel) {
 
 					ngModel.$validators.absenceOfForbiddenCharacters = function(value) {
 						return /^[\s!-ϿԱ-֏ἀ-῾\u2003₠-₾]+$/i.test(value);
