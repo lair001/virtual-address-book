c00d3a1c820c1549ee7b3425e023045aba9acf30
Samuel Lair <lair001@gmail.com>
2017-01-23 23:44:19 -0500

    implemented absence of whitespace directive

---
+++ app/assets/javascripts/angularApp/directives/validations/absenceOfWhitespace.js
@@ -0,0 +1,23 @@
+(function() {
+
+	angular
+		.module('app')
+		.directive('absenceOfWhitespace', [function absenceOfWhitespace() {
+
+			return {
+
+				restrict: 'A',
+				require: 'ngModel',
+				link: function(scope, element, attrs, ngModel) {
+
+					ngModel.$validators.absenceOfWhitespace = function(value) {
+						return /^[^\t\f\v\n\r \u00A0…\u2003]+$/i.test(value);
+					};
+
+				}
+
+			};
+
+		}]);
+
+})();
