4e1ff718de54af6708084586c722108a99f4de91
Samuel Lair <lair001@gmail.com>
2017-01-25 22:41:40 -0500

    adjusted validation directives to account for blank entries

--- app/assets/javascripts/angularApp/directives/validations/CityRegex.js
+++ app/assets/javascripts/angularApp/directives/validations/CityRegex.js
@@ -11,7 +11,7 @@
 				link: function(scope, element, attrs, ngModel) {
 
 					ngModel.$validators.cityRegex = function(value) {
-						return /^[\w .]{,25}$/i.test(value);
+						return /^([\w .]{,25}|)$/i.test(value);
 					};
 
 				}
