dbe29eb571ee2dca7b6a44284627b6b6f58434b5
Samuel Lair <lair001@gmail.com>
2017-01-25 21:03:06 -0500

    implemented OccupationRegex directive

---
+++ app/assets/javascripts/angularApp/directives/validations/OccupationRegex.js
@@ -0,0 +1,23 @@
+(function() {
+
+	angular
+		.module('app')
+		.directive('occupationRegex', [function OccupationRegex() {
+
+			return {
+
+				restrict: 'A',
+				require: 'ngModel',
+				link: function(scope, element, attrs, ngModel) {
+
+					ngModel.$validators.occupationRegex = function(value) {
+						return /^\w{,25}$/i.test(value);
+					};
+
+				}
+
+			};
+
+		}]);
+
+})();
