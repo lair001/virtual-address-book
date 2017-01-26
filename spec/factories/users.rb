4ab5fb51c85b654dc19a2bc1283f4308eb9c6d76
Samuel Lair <lair001@gmail.com>
2017-01-26 04:42:45 -0500

    implemented user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,33 @@
 FactoryGirl.define do
-  factory :user do
-    username "MyString"
-    email "MyString"
-    password_digest "MyString"
-    security_code "MyString"
-    role 1
-  end
+	factory :user do
+		sequence(:username) { |n| "lair00#{n}" }
+		sequence(:email) { |n| "#{username}@example.com" }
+		password "password"
+		created_at { 10.days.ago }
+		updated_at { Time.now }
+
+		trait :true_random do
+			username { fake_username }
+			email { "#{username}@#{fake_domain}" }
+			password { fake_password }
+			created_at { Faker::Time.between(5.years.ago, 10.days.ago, :all) }
+			updated_at { Faker::Time.between(4.days.ago, Time.now, :all) }
+		end
+
+		trait :random do
+			username { fake_username }
+			email { "#{username}@#{fake_domain}" }
+			created_at { Faker::Time.between(5.years.ago, 10.days.ago, :all) }
+			updated_at { Faker::Time.between(4.days.ago, Time.now, :all) }
+		end
+
+		trait :banned do
+			role 'banned'
+		end
+
+		trait :administrator do
+			role 'administrator'
+		end
+
+	end
 end
