03c6dd29cdd924d68bd91844a25736a0e0650c0c
Samuel Lair <lair001@gmail.com>
2017-01-25 21:05:13 -0500

    implemented employer regex directive

---
+++ app/assets/javascripts/angularApp/directives/validations/EmployerRegex.js
@@ -0,0 +1,23 @@
+(function() {
+
+	angular
+		.module('app')
+		.directive('employerRegex', [function EmployerRegex() {
+
+			return {
+
+				restrict: 'A',
+				require: 'ngModel',
+				link: function(scope, element, attrs, ngModel) {
+
+					ngModel.$validators.employerRegex = function(value) {
+						return /^\w{,50}$/i.test(value);
+					};
+
+				}
+
+			};
+
+		}]);
+
+})();
