c27034196bbee6cf210878d7c1b2051c4969361e
Samuel Lair <lair001@gmail.com>
2017-01-25 21:08:25 -0500

    implemented contact address line regex directive

---
+++ app/assets/javascripts/angularApp/directives/validations/ContactAddressLineRegex.js
@@ -0,0 +1,23 @@
+(function() {
+
+	angular
+		.module('app')
+		.directive('contactAddressLineRegex', [function ContactAddressLineRegex() {
+
+			return {
+
+				restrict: 'A',
+				require: 'ngModel',
+				link: function(scope, element, attrs, ngModel) {
+
+					ngModel.$validators.contactAddressLineRegex = function(value) {
+						return /^[\w .]{,50}$/i.test(value);
+					};
+
+				}
+
+			};
+
+		}]);
+
+})();
