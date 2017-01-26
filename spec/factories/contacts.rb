07b4df4b7bbb407281a954371b6464bb19f16351
Samuel Lair <lair001@gmail.com>
2017-01-26 05:19:36 -0500

    implemented contacts factory and associated Faker helper methods

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -1,14 +1,18 @@
 FactoryGirl.define do
-  factory :contact do
-    user_id 1
-    last_name "MyString"
-    first_name "MyString"
-    email "MyString"
-    phone "MyString"
-    address_line_1 "MyString"
-    address_line_2 "MyString"
-    city "MyString"
-    state "MyString"
-    zip_code "MyString"
-  end
+    factory :contact do
+
+        last_name { fake_last_name }
+        first_name { fake_first_name }
+        occupation { fake_occupation }
+        employer { fake_employer }
+        email { fake_email }
+        phone { fake_phone }
+        address_line_1 { fake_address_line_1 }
+        address_line_2 { fake_address_line_2 }
+        city { fake_city }
+        state { fake_state }
+        zip_code { fake_zip_code }
+
+        association :user
+    end
 end
--- spec/spec_helper/config/faker/faker_spec_helper.rb
+++ spec/spec_helper/config/faker/faker_spec_helper.rb
@@ -21,12 +21,56 @@ module FakerSpecHelper
 			Faker::Internet.password(Devise.password_length.min, Devise.password_length.max)
 		end
 
-		def fake_email
+		def fake_unique_email
 			return unique_value(-> { Faker::Internet.safe_email }, User, :email )
+		end
+
+		def fake_phone
+			"(#{rand(9)+1}#{rand(10)}#{rand(10)})#{rand(10)}#{rand(10)}#{rand(10)}-#{rand(10)}#{rand(10)}#{rand(10)}#{rand(10)}"
+		end
+
+		def fake_address_line_1
+			Faker::Address.street_address
+		end
+
+		def fake_address_line_2
+			Faker::Address.secondary_address
+		end
+
+		def fake_city
+			Faker::Address.city
+		end
+
+		def fake_state
+			Faker::Address.state_abbr
+		end
+
+		def fake_zip_code
+			Faker::Address.zip_code
+		end
+
+		def fake_email
+			Faker::Internet.safe_email
 		end
 
 		def fake_domain
 			Faker::Internet.domain_name
+		end
+
+		def fake_occupation
+			Faker::Company.profession
+		end
+
+		def fake_employer
+			Faker::Company.name
+		end
+
+		def fake_last_name
+			Faker::Name.last_name
+		end
+
+		def fake_first_name
+			Faker::Name.first_name
 		end
 
 		def unique_value(value_generator_lambda, model_class, model_attribute)
