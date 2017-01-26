95078e97595c74039a04884f3e9b7bb5c4d20e52
Samuel Lair <lair001@gmail.com>
2017-01-26 06:03:16 -0500

    whitelisting apostrophes in contact name regex

--- app/assets/javascripts/angularApp/directives/validations/ContactNameRegex.js
+++ app/assets/javascripts/angularApp/directives/validations/ContactNameRegex.js
@@ -11,7 +11,7 @@
 				link: function(scope, element, attrs, ngModel) {
 
 					ngModel.$validators.contactNameRegex = function(value) {
-						return /^\w{1,25}$/i.test(value);
+						return /^[\w']{1,25}$/i.test(value);
 					};
 
 				}
