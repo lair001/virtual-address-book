4ec9b7d447744fdb1cb547f6469ffec5f8d07bd6
Samuel Lair <lair001@gmail.com>
2017-01-26 01:07:50 -0500

    rearranged contacts view and its nested views

--- app/assets/javascripts/angularApp/controllers/index/signed_in/contacts/ContactsShowController.js
+++ app/assets/javascripts/angularApp/controllers/index/signed_in/contacts/ContactsShowController.js
@@ -36,6 +36,25 @@
 				}
 			};
 
+			contactsShow.submitEdit = function contactsShowSubmitEdit($event) {
+				$event.preventDefault();
+				$http({
+					method: "PATCH",
+					data: { contact: contactsShow.contact},
+					url: "/contacts/" + contactsShow.contact.id
+				}).then(function onSuccessfulContactsShowEdit(response) {
+					// signIn.errorTitle = undefined;
+					// signIn.user = Object.assign({}, signIn.newUser);;
+					parentContacts.splice(contactsShow.contactsIndex, 1, response.data);
+					contactsShow.errorTitle = undefined;
+					contactsShow.errorsDetail = undefined;
+					$state.go('index.signed_in.contacts.show', { id: response.data.id });
+				}, function onFailedContactsShowEdit(response) {
+					contactsShow.errorTitle = response.data.errors[0].title;
+					contactsShow.errorsDetail = response.data.errors[0].detail;
+				});
+			};
+
 			// begin partial config
 			$scope.controller = contactsShow;
 			contactsShow.locals = {
